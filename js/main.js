5ca228277674c7db6ff30d1500bb3d0366e15481
Albert Volkman <albert.volkman@gmail.com>
2017-03-01 12:38:39 -0500

    Convert to non-https

--- js/main.js
+++ js/main.js
@@ -223,7 +223,7 @@ function centerContent()
 -----------------------------------------------------------*/
 
 var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/player_api";
+tag.src = "http://www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
