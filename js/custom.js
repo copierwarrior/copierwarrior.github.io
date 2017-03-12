5675f2d3ea60b0cbdd0e191dfb1f5d012774143d
Albert Volkman <albert.volkman@gmail.com>
2017-03-11 23:19:36 -0500

    Execute waypoint actions once

--- js/custom.js
+++ js/custom.js
@@ -51,6 +51,7 @@
     $('#counter-3').animateNumber({
       number: 999
     }, 2000);
+    this.destroy()
   }, {
     offset: '100%'
   });
@@ -68,6 +69,7 @@
     $('#expand-counter-4').animateNumber({
       number: 67
     }, 3000);
+    this.destroy()
   }, {
     offset: '100%'
   });
@@ -77,62 +79,74 @@
     $('.expand-2').css('width', '72%');
     $('.expand-3').css('width', '86%');
     $('.expand-4').css('width', '64%');
+    this.destroy()
   }, {
     offset: '100%'
   });
 
   $('.wp1').waypoint(function() {
     $('.wp1').addClass('animated fadeInDown');
+    this.destroy()
   }, {
     offset: '75%'
   });
   $('.wp2').waypoint(function() {
     $('.wp2').addClass('animated fadeInUp');
+    this.destroy()
   }, {
     offset: '75%'
   });
   $('.wp3').waypoint(function() {
     $('.wp3').addClass('animated fadeInDown');
+    this.destroy()
   }, {
     offset: '55%'
   });
   $('.wp4').waypoint(function() {
     $('.wp4').addClass('animated fadeInUp');
+    this.destroy()
   }, {
     offset: '75%'
   });
   $('.wp5').waypoint(function() {
     $('.wp5').addClass('animated fadeInUp');
+    this.destroy()
   }, {
     offset: '75%'
   });
   $('.wp6').waypoint(function() {
     $('.wp6').addClass('animated fadeInUp');
+    this.destroy()
   }, {
     offset: '75%'
   });
   $('.wp7').waypoint(function() {
     $('.wp7').addClass('animated fadeInDown');
+    this.destroy()
   }, {
     offset: '75%'
   });
   $('.wp8').waypoint(function() {
     $('.wp8').addClass('animated fadeInUp');
+    this.destroy()
   }, {
     offset: '100%'
   });
   $('.wp9').waypoint(function() {
     $('.wp9').addClass('animated fadeInDown');
+    this.destroy()
   }, {
     offset: '100%'
   });
   $('.wp10').waypoint(function() {
     $('.wp10').addClass('animated fadeInLeft');
+    this.destroy()
   }, {
     offset: '100%'
   });
   $('.wp11').waypoint(function() {
     $('.wp11').addClass('animated fadeInUp');
+    this.destroy()
   }, {
     offset: '75%'
   });
