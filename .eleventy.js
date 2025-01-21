d072ca590641040b87bf98aeb06f24a39e62407a
Albert Volkman <albert.volkman@gmail.com>
2025-01-20 21:10:04 -0500

    Simplify config

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 export default function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('css');
-  eleventyConfig.addPassthroughCopy({ 'node_modules/font-awesome/fonts': 'fonts' });
-  eleventyConfig.addPassthroughCopy({ 'node_modules/bootstrap-sass/assets/fonts/bootstrap': 'fonts/bootstrap' });
-  eleventyConfig.addPassthroughCopy('favicon');
-  eleventyConfig.addPassthroughCopy('img');
-  eleventyConfig.addPassthroughCopy('js');
+  eleventyConfig.addPassthroughCopy({
+    'css': 'css',
+    'favicon': 'favicon',
+    'img': 'img',
+    'js': 'js',
+    'node_modules/font-awesome/fonts': 'fonts',
+    'node_modules/bootstrap-sass/assets/fonts/bootstrap': 'fonts/bootstrap',
+  });
   eleventyConfig.setIncludesDirectory('_includes');
   eleventyConfig.setLayoutsDirectory('_layouts');
 
